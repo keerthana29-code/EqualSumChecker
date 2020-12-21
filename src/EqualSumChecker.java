b889bed7e4aa018324e87b6491a341b9b30cb7ce
keerthana29-code <keerthichalapathi29@gmail.com>
2020-12-21 20:20:12 +0530

    Initial commit

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,17 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int a, int b, int c){
+
+        if(a+b==c){
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+        }
+
+
+
